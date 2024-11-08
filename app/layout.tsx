800d26da282003f5faaeca8987ee8f467f7462da
Eunji <eeunjiilee@gmail.com>
2024-11-08 20:49:51 +0900

    refactor: restructure project directory layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,7 +15,11 @@ export const metadata: Metadata = {
   description: '이은지의 포트폴리오',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
   return (
     <html lang="ko" className={`${pretendard.variable}`}>
       <body className={pretendard.className}>{children}</body>
