3d14d5ab8846ba19316a272840f2abcb571b26a4
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2024-11-10 15:06:02 +0900

    feat: add css vars (#13)
    * feat: add project dynamic route
    
    * feat: add css vars

--- src/components/ui/Callout/index.css.ts
+++ src/components/ui/Callout/index.css.ts
@@ -2,7 +2,7 @@ import { style } from '@vanilla-extract/css';
 
 export const callout = style({
   padding: '24px',
-  background: 'var(--color-background-callout)',
+  background: 'var(--color-bg-callout)',
   border: '1px solid var(--color-border)',
   borderRadius: '24px',
 });
