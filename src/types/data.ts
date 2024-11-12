cacdb7cdae5c8ffde613b239014c1abf788ff12f
Eunji Lee <77034159+eunjios@users.noreply.github.com>
2024-11-12 22:01:45 +0900

    feat: get project data from firebase (#24)
    * chore: add firebase to dependencies
    
    * feat: add firebase config
    
    * feat: define project data type
    
    * feat: create function to get project from firebase function

--- src/types/data.ts
+++ src/types/data.ts
@@ -10,3 +10,25 @@ export interface ProjectDetail {
   title: string;
   texts: string[];
 }
+
+export interface ProjectData {
+  title: string;
+  about: string;
+  company: string;
+  period: {
+    start: string;
+    end: string;
+  };
+  role: string;
+  team: string;
+  techStack: string;
+  features: string[];
+  contributions: {
+    title: string;
+    details: string[];
+  }[];
+  troubleshooting: {
+    title: string;
+    details: string[];
+  }[];
+}
