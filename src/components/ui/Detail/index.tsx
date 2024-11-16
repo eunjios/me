2532a18e2973d15e893786c930fe6b521cfdc923
Eunji <eeunjiilee@gmail.com>
2024-11-16 13:30:59 +0900

    feat: update components used in project detail page
    - conditional rendering
    - add links data

--- src/components/ui/Detail/index.tsx
+++ src/components/ui/Detail/index.tsx
@@ -1,14 +1,17 @@
 import { Bullet } from '../Bullet';
+import { ExternalLink } from '../ExternalLink';
 import { Text } from '../Text';
+import { LinkData } from '@/types/data';
 
 import { pdBottom, txt } from './index.css';
 
 interface Props {
   title?: string;
   texts: string[];
+  links?: LinkData[];
 }
 
-export const Detail = ({ title, texts }: Props) => {
+export const Detail = ({ title, texts, links }: Props) => {
   return (
     <div className={pdBottom}>
       {title && <Text className={txt}>{title}</Text>}
@@ -16,6 +19,13 @@ export const Detail = ({ title, texts }: Props) => {
         {texts.map((text, index) => (
           <Bullet.Item key={index}>{text}</Bullet.Item>
         ))}
+        {links && (
+          <Bullet.Item>
+            {links?.map((link, index) => (
+              <ExternalLink key={index} href={link.link} text={link.title} />
+            ))}
+          </Bullet.Item>
+        )}
       </Bullet>
     </div>
   );
